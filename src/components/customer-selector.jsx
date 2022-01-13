09c3bb99ab2cd8c3c9b026333c3dca5a62bce5b7
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-01-13 16:43:01 -0600

    clear customer autocomplete when new button is pressed

--- src/components/customer-selector.jsx
+++ src/components/customer-selector.jsx
@@ -89,6 +89,7 @@ export default function CustomerSelector({ setCustomer }) {
                     }
                     setShowForm(true);
                     setInput("");
+                    setCustomers([]);
                 }}>
                     New
                 </Button>
